537e04caa53490550bebc99855f98bb3adea5035
midbezoar <2974490550@qq.com>
2022-08-16 17:26:59 +0800

    创建 Icon 组件，nav 样式

---
+++ src/components/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="Icon">
+    <use :xlink:href="'#' + name" />
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['name'],
+  name: 'Icon',
+}
+</script>
+
+<style lang="scss" scoped>
+.Icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,53 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">记账</router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-  </div>
+  <nav class="nav">
+    <router-link to="/money" class="item">
+      <Icon name="money" />
+      记账
+    </router-link>
+    <router-link to="/labels" class="item">
+      <Icon name="label" />
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item">
+      <Icon name="statistics" />
+      统计
+    </router-link>
+  </nav>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+// eslint-disable-next-line no-undef
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => {
+  requireContext.keys().forEach(requireContext)
+}
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/))
+} catch (error) {
+  console.log(error)
+}
+
+export default {
+  name: 'Nav',
+}
 </script>
 
 <style lang="scss" scoped>
-.nav{
-  
+nav {
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 0 3px rgb(0, 0, 0, 0.25);
+  font-size: 12px;
+  > .item{
+    padding: 2px 0;
+    width: 33.3333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .Icon {
+      width: 30px;
+      height: 30px;
+    }
+  }
 }
 </style>
