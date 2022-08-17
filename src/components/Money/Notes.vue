ea76f00c051aac16fcf7b18979143df027335ffc
midbezoar <2974490550@qq.com>
2022-08-17 22:54:16 +0800

    模块化

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="在这里输入备注" />
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Notes',
+}
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  display: block;
+  padding-left: 14px;
+  display: flex;
+  align-items: center;
+  input {
+    height: 64px;
+    flex: 1;
+    background: transparent;
+    border: none;
+    padding: 0 16px;
+  }
+}
+</style>
