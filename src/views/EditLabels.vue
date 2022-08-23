8a4ec3bb6fdf91946cb3f18b83caca37321b833c
midbezoar <2974490550@qq.com>
2022-08-23 23:01:24 +0800

    创建 editLabels

---
+++ src/views/EditLabels.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class Labels extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
