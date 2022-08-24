6947314c5e3be1c216cab8bc6a6af40756326234
midbezoar <2974490550@qq.com>
2022-08-24 15:03:09 +0800

    新增 editLabels 组件

--- src/views/EditLabels.vue
+++ src/views/EditLabels.vue
@@ -1,15 +1,26 @@
 <template>
-  <Layout>
-    
-  </Layout>
+  <Layout> </Layout>
 </template>
 
 <script lang="ts">
+import tagListModel from '@/models/tagListModel'
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 
 @Component
-export default class Labels extends Vue {}
+export default class Labels extends Vue {
+  created() {
+    const id = this.$route.params.id
+    tagListModel.fetch()
+    const tags = tagListModel.data
+    const tag = tags.filter((t) => t.id === id)[0]
+    if (tag) {
+      console.log(tag)
+    } else {
+      this.$router.replace('/404')
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
