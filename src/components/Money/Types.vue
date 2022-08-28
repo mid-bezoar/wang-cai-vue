632e382eefd208aadc62146944472bb69d7fef9f
midbezoar <2974490550@qq.com>
2022-08-28 21:18:40 +0800

    Statistics

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="value === '-' && 'selected'" @click="selectType('-')">支出</li>
-      <li :class="value === '+' && 'selected'" @click="selectType('+')">收入</li>
+      <li :class="{ selected: value === '-', [classPrefix + '-item']: classPrefix }" @click="selectType('-')">支出</li>
+      <li :class="{ selected: value === '+', [classPrefix + '-item']: classPrefix }" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
@@ -14,6 +14,7 @@ import { Component, Prop } from 'vue-property-decorator'
 @Component
 export default class Types extends Vue {
   @Prop(String) value!: string
+  @Prop(String) classPrefix?: string
 
   selectType(type: string) {
     if (type !== '-' && type !== '+') {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,11 +1,27 @@
 <template>
-  <Layout> statistics.Vue </Layout>
+  <Layout>
+    <Types class="x" :value.sync="yyy" />
+  </Layout>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Statistics',
+import Vue from 'vue'
+import Types from '@/components/Money/Types.vue'
+import { Component } from 'vue-property-decorator'
+
+@Component({
+  components: { Types }
+})
+export default class Statistics extends Vue {
+  yyy = '-'
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.x ::v-deep li {
+  background: white;
+  &.selected {
+    background: #c4c4c4;
+  }
+}
+</style>
